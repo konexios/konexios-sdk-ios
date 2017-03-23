fbb6b4f0019a59d9a9d95bbc8b64ab958e340383
Michael Kalinin <mkalinin@mera.ru>
2017-03-23 13:53:45 +0300

    KR-1506: Add copyright

--- AcnSDKSample/AcnSDKSample/LoginViewController.swift
+++ AcnSDKSample/AcnSDKSample/LoginViewController.swift
@@ -2,8 +2,13 @@
 //  LoginViewController.swift
 //  AcnSDKSample
 //
-//  Created by Michael Kalinin on 24/11/2016.
-//  Copyright © 2016 Arrow. All rights reserved.
+//  Copyright (c) 2017 Arrow Electronics, Inc.
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Apache License 2.0
+//  which accompanies this distribution, and is available at
+//  http://apache.org/licenses/LICENSE-2.0
+//
+//  Contributors: Arrow Electronics, Inc.
 //
 
 import UIKit
