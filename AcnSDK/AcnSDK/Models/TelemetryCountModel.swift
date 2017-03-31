1daddc8240f6ee86f23d1d8bb4ca882ee073c977
Michael Kalinin <mkalinin@mera.ru>
2017-03-31 16:06:33 +0300

    KR-1506: Change copyright

--- AcnSDK/AcnSDK/Models/TelemetryCountModel.swift
+++ AcnSDK/AcnSDK/Models/TelemetryCountModel.swift
@@ -2,8 +2,13 @@
 //  TelemetryCountModel.swift
 //  AcnSDK
 //
-//  Created by Michael Kalinin on 31/03/2017.
-//  Copyright © 2017 Arrow. All rights reserved.
+//  Copyright (c) 2017 Arrow Electronics, Inc.
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Apache License 2.0
+//  which accompanies this distribution, and is available at
+//  http://apache.org/licenses/LICENSE-2.0
+//
+//  Contributors: Arrow Electronics, Inc.
 //
 
 import Foundation
