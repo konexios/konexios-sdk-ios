74ec218ad16cf771e03158fe015858c21f11fcf5
Michael Kalinin <mkalinin@mera.ru>
2017-04-24 15:08:29 +0300

    KR-1590: Device state API implementation

---
+++ AcnSDK/AcnSDK/Models/StateModel.swift
@@ -0,0 +1,32 @@
+//
+//  StateModel.swift
+//  AcnSDK
+//
+//  Copyright (c) 2017 Arrow Electronics, Inc.
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Apache License 2.0
+//  which accompanies this distribution, and is available at
+//  http://apache.org/licenses/LICENSE-2.0
+//
+//  Contributors: Arrow Electronics, Inc.
+//
+
+import Foundation
+
+public class StateModel: BaseCloudModel {
+    
+    public var timestamp: String
+    public var states: [String : String]
+    
+    override var params: [String : AnyObject] {
+        return [
+            "timestamp" : timestamp as AnyObject,
+            "states"    : states as AnyObject
+        ]
+    }
+    
+    public init (states: [String : String]) {
+        self.timestamp = Date().formatted
+        self.states = states
+    }    
+}
