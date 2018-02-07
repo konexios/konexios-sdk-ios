eff80d4df574e2595f367e0d3e3f60134fcf5bba
Aleksei Chechetkin <achechet@mera.ru>
2018-02-07 14:09:39 +0300

    Migration to Swift4 with full bitcode support

--- AcnSDK/Third-party/Alamofire/Source/Request.swift
+++ AcnSDK/Third-party/Alamofire/Source/Request.swift
@@ -305,7 +305,8 @@ extension Request: CustomDebugStringConvertible {
                 let cookies = cookieStorage.cookies(for: url), !cookies.isEmpty
             {
                 let string = cookies.reduce("") { $0 + "\($1.name)=\($1.value);" }
-                components.append("-b \"\(string.substring(to: string.index(before: string.endIndex)))\"")
+                let substr = string[..<(string.index(before: string.endIndex))]
+                components.append("-b \"\(substr)\"")
             }
         }
 
